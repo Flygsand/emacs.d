7870861576290e81379a9d8289bbea066bb552c4
Martin Häger <martin.haeger@gmail.com>
2009-11-04 22:07:47 +0100

    Added general configuration section to init.el

--- init.el
+++ init.el
@@ -1,4 +1,35 @@
 ;;----------------------------------------------------------------------------
+;; General configuration
+;;----------------------------------------------------------------------------
+
+; disable menu bar, tool bar, scroll bar and blinking cursor
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+
+; use UTF-8
+(prefer-coding-system 'utf-8)
+ 
+; Lines shouldn't be longer than 79 chars
+(setq fill-column 72)
+ 
+; shows current selected region
+(setq-default transient-mark-mode t)
+ 
+; indent via spaces not tabs
+(setq-default indent-tabs-mode nil)
+ 
+; titlebar = buffer unless filename
+(setq frame-title-format '(buffer-file-name "%f" ("%b")))
+ 
+; show paired parenthesis
+(show-paren-mode 1)
+ 
+; TAB => 4*'\b'
+(setq default-tab-width 4)
+
+;;----------------------------------------------------------------------------
 ;; Set load path
 ;;----------------------------------------------------------------------------
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
@@ -17,14 +48,6 @@
 (require 'find-func)
 (defun path-to-library (library-name)
   (file-name-as-directory (file-name-directory (find-library-name library-name))))
-
-;;----------------------------------------------------------------------------
-;; UI tweaks
-;;----------------------------------------------------------------------------
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(blink-cursor-mode -1)
 
 ;;----------------------------------------------------------------------------
 ;; Color theme
@@ -51,4 +74,3 @@
 ;; Python
 ;;----------------------------------------------------------------------------
 (require 'init-python-mode)
-
