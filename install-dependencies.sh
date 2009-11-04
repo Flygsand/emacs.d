a4c8e30cd0d44f85cc5f0bb802c696e890ba8d1b
Martin Häger <martin.haeger@gmail.com>
2009-11-04 22:26:08 +0100

    Added script for installing dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# install needed Arch packages
+yaourt -Sy setuptools mercurial pymacs pyflakes
+
+cd /tmp
+
+# fetch sources
+hg clone http://bitbucket.org/agr/rope
+hg clone http://bitbucket.org/agr/ropemacs
+hg clone http://bitbucket.org/agr/ropemode
+
+# install rope
+sudo easy_install rope
+
+# install ropemacs
+ln -s ../ropemode/ropemode ropemacs/
+
+sudo easy_install ropemacs
+
+# clean up
+sudo rm -rf rope ropemacs ropemode
+
